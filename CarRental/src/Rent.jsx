676a5c266655c8ddd62fa58eda952fe64ed55fe6
karanvir singh <karanvirsingh141414@gmail.com>
2026-01-27 22:54:30 +0530

    replace problematic Carousel with new CarouselSlider component

--- CarRental/src/Rent.jsx
+++ CarRental/src/Rent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header';
-import Carousel_Component from './components/Carousel';
+import CarouselSlider from './components/CarouselSlider';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Collections from './components/Collections';
 import Footer from './components/Footer';
@@ -12,7 +12,7 @@ function Rent() {
   return (
     <>
     <Header/>
-    <Carousel_Component/>
+    <CarouselSlider/>
     <Collections/>
     <Footer/>
   </>
